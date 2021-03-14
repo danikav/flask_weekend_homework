28b63ad4a3dcdbb59c7ea3ff5e5c2692fa7dbd9f
Dani <danikav1989@gmail.com>
2021-03-14 23:11:02 +0000

    MVP done

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -2,9 +2,17 @@ from flask import render_template
 
 from app import app
 from app.models.player import Player
+from app.models.game import Game
+
+@app.route('/')
+
 
 @app.route('/<choice_1>/<choice_2>')
-def play_game(choice_1, choice_2):
+def play_the_game(choice_1, choice_2):
     player_1 = Player("Player 1", choice_1)
     player_2 = Player("Player 2", choice_2)
-    return f"Player 1 chose: {choice_1} Player 2 chose: {choice_2} "
+    game = Game(player_1, player_2)
+    winner = game.play_game(choice_1, choice_2)
+    return f"Player 1 chose: {choice_1} Player 2 chose: {choice_2} . So, the result is... {winner}"
+
+@app.route
--- app/models/game.py
+++ app/models/game.py
@@ -1,2 +1,22 @@
-from player import Player
+class Game():
+    def __init__(self, player_1, player_2):
+        self.player_1 = player_1
+        self.player_2 = player_2
+
+    def play_game(self, choice_1, choice_2):
+        if choice_1 == choice_2:
+            return "a draw..."
+        elif choice_1 == "rock" and choice_2 == "scissors":
+                return "Player 1 wins!"
+        elif choice_1 == "rock" and choice_2 == "paper":
+                return "Player 2 wins!"
+        elif choice_1 == "scissors" and choice_2 == "rock":
+                return "Player 2 wins!"
+        elif choice_1 == "scissors" and choice_2 == "paper":
+                return "Player 1 wins!"
+        elif choice_1 == "paper" and choice_2 == "scissors":
+                return "Player 2 wins!"   
+        elif choice_1 == "paper" and choice_2 == "rock":
+                return "Player 1 wins!"   
+        
 
